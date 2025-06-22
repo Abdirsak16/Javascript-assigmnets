ad95b64ca63b1e5e3f097ab55b475efdf018eaed
Abdirsak16 <abdirsakabdulla@gmail.com>
2025-06-22 12:04:45 +0300

    assigment 2 and 3

---
+++ assigment2/main.js
@@ -0,0 +1,18 @@
+let a = 15;
+let b = "15";
+console.log('equal to')
+console.log(a==b);
+console.log('not equal to')
+console.log(a!=b);
+console.log('Srictly equal to')
+console.log(a===b);
+console.log('Not strictly equal to')
+console.log(a!==b);
+console.log('Greater than')
+console.log(a>b);
+console.log('Less than ')
+console.log(a<b);
+console.log('Greater than or equal to')
+console.log(a>=b);
+console.log('Less than or equal to')
+console.log(a<=b);
---
+++ jsexercise1/main.js
@@ -0,0 +1,24 @@
+let x=7;
+let y=3;
+console.log("addition")
+console.log(x+y);
+console.log("Substraction")
+console.log(x-y);
+console.log("Multiplication")
+console.log(x*y);
+console.log("Division")
+console.log(x/y);
+console.log("Modular")
+console.log(x%y);
+console.log("Pre Increment")
+console.log(++x);
+
+console.log("Post increment")
+console.log(x++);
+
+console.log("pre increment")
+console.log(--y);
+
+console.log("Post increment")
+console.log(y--);
+
