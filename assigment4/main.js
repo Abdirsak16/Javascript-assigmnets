42c55d85bd6ffd32b6ab794a7664f248957fd3bc
Abdirsak16 <abdirsakabdulla@gmail.com>
2025-06-23 13:04:38 +0300

    assigment four is added

---
+++ assigment4/main.js
@@ -0,0 +1,16 @@
+
+//Function basic 
+function Add(x,y){
+    console.log(x+y);
+}
+Add(4,8);
+Add(10,10)
+
+//Function with expresion 
+const Add = function(a,b){
+    return a+b;
+}
+console.log(Add(10,10))
+console.log(Add(2,2))
+const Final=Add(10,2);
+console.log(Final/2)
