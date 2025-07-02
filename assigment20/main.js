c295c174b4c0dc8bf1d0324d44b1ae9e9bbbb8ac
Abdirsak16 <abdirsakabdulla@gmail.com>
2025-07-02 12:09:56 +0300

    assigment 19 ,20 21 are added

---
+++ assigment20/main.js
@@ -0,0 +1,5 @@
+
+
+const fruit=["apple","banana","cherry"];
+const newfrius = fruit.map ((fruit) => fruit.length);
+console.log( newfrius );
---
+++ assigment21/main.js
@@ -0,0 +1,6 @@
+
+
+/// Reduce method
+const number=[1,2,3,4,5]
+const multipication= number.reduce((total,numb) => total * numb ,1);
+console.log(multipication)
