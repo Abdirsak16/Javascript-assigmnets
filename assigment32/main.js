75b4c1c45548171d79eb02ad9a724fdcb9ed664e
Abdirsak16 <abdirsakabdulla@gmail.com>
2025-07-27 18:11:17 +0300

    added exercises32 and 33

---
+++ assigment32/main.js
@@ -0,0 +1,6 @@
+// exerice 32 use querySelector and querySelectorAll
+const title=document.getElementsByTagName('h2');
+const text=document.querySelectorAll('.text')
+
+console.log(title)
+console.log(text)
---
+++ assigment33/main.js
@@ -0,0 +1,16 @@
+//Change the contenct
+
+
+
+function changeText(){
+const header=document.querySelector('h2');
+const hoppy=document.querySelector('.hobby');
+const p2=document.querySelectorAll('.hobby')[1];
+
+   header.textContent = `i became  Developer`;
+   hoppy.innerHTML="make tge change the first paragrah";
+   p2.innerHTML= "   <span>  this the <strong> second paragraph</strong></span>"
+
+
+}
+
